5f502d1d8243600a0cb7af3131b47e9f3b37a660
Steglasaurous <me@steglasaurous.com>
2024-09-25 20:09:26 -0400

    Clean up synth riders modifiers template

--- src/app/component/game-specific/synth-riders-modifiers/synth-riders-modifiers.component.ts
+++ src/app/component/game-specific/synth-riders-modifiers/synth-riders-modifiers.component.ts
@@ -7,11 +7,52 @@ import {initialState} from "../../../state/gamestate.reducer";
   templateUrl: './synth-riders-modifiers.component.html',
   styleUrls: ['./synth-riders-modifiers.component.scss']
 })
-export class SynthRidersModifiersComponent implements OnInit {
+export class SynthRidersModifiersComponent {
   @Input()
   gameState: GameStateModel = initialState;
 
-  ngOnInit(): void {
+  getSpinDegrees(spinDegreesValue: number): string {
+    switch (spinDegreesValue) {
+      case 0:
+        return '90';
+      case 1:
+        return '180';
+      case 2:
+        return '360';
+      case 3:
+        return '360+';
+    }
+
+    return '';
   }
+
+  getSpinIntensity(spinIntensityValue: number): string {
+    switch (spinIntensityValue) {
+      case 0:
+        return 'styled';
+      case 1:
+        return 'wild';
+      case 2:
+        return 'mild';
+    }
+
+    return '';
+  }
+
+  getSpiralIntensity(spiralIntensityValue: number): string {
+    switch (spiralIntensityValue) {
+      case 1:
+        return 'mild';
+      case 2:
+        return 'styled';
+      case 3:
+        return 'wild';
+    }
+
+    return '';
+  }
+
+
+
 
 }
